cdc8e2a422afe4d2a8082e4fdfb8677eb02c4e1f
helio.barros <helio.barros@saude.gov.br>
2025-03-10 14:32:45 -0300

    Rotas com parametros

--- conceitos_nest/src/recados/recados.controller.ts
+++ conceitos_nest/src/recados/recados.controller.ts
@@ -1,12 +1,25 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+
+
+
+// CRUD
+// Create -> POST -> Criar um recado
+// Read -> GET -> Ler todos os recados
+// Read -> GET -> Ler apenas um recado
+// Update -> PATCH / PUT -> Atualizar um recado
+// Delete -> DELETE -> Apagar um recado
+
+// PATCH é utilizado para atualizar dados de um recurso
+// PUT é utilizado para atualizar um recurso inteiro
 
 @Controller('recados')
 export class RecadosController {
 
     // Encontrar todos os recados
     @Get()
-    findAll() {
-        return 'Essa rota retorna todos os recados!!';
+    findAll(@Query() pagination: any) {
+        const {limit = 10, offset = 0} = pagination;
+        return `Essa rota retorna todos os recados. Limit= ${limit} , Offset= ${offset}`;
     }
 
     // Encontrar Um recado
@@ -19,6 +32,19 @@ export class RecadosController {
     @Post()
     create(@Body() body: any){
         return  body;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body: any){
+        return {
+            id,
+            ...body
+        }
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string){
+        return `Essa rota APAGA o recado ID ${id}`
     }
 
     
