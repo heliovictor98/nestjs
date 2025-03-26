7d181e4c409939b1ca45453e6b8e8f2d05470b71
helio.barros <helio.barros@saude.gov.br>
2025-03-26 12:07:47 -0300

    Config Database postgres

--- conceitos_nest/src/app/app.module.ts
+++ conceitos_nest/src/app/app.module.ts
@@ -4,9 +4,20 @@ import { AppService } from './app.service';
 import { ConceitosManualModule } from 'src/conceitos-manual/conceitos-manual.module';
 import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
 import { RecadosModule } from 'src/recados/recados.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      database: 'postgres',
+      password: '123456',
+      autoLoadEntities: true, // Carrega entidades sem precisar espeficialas
+      synchronize: true,
+    }),
     RecadosModule
   ],
   controllers: [AppController],
